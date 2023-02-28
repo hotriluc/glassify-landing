fdf1192534666bd9a81753d4549b9b1cdecbbf0a
Luc Ho <hotriluc97@gmail.com>
2023-02-28 18:43:30 +0100

    home section basic layout

--- src/components/home/HomeOverlay.jsx
+++ src/components/home/HomeOverlay.jsx
@@ -4,9 +4,11 @@ import classes from "./HomeOverlay.module.css";
 const HomeOverlay = () => {
   return (
     <section className={classes.home}>
-      <h1>The eternity is our craft. Glassify.</h1>
-
-      <p>
+      <h1 className={classes.heading}>
+        The eternity is our craft. <br />
+        Glassify.
+      </h1>
+      <p className={classes.text}>
         Lorem ipsum dolor sit amet consectetur, adipisicing elit. Non
         repudiandae fugiat, et ab, voluptatum amet consectetur sunt enim ad ut,
         rem omnis reiciendis cumque necessitatibus soluta iusto aliquam beatae
