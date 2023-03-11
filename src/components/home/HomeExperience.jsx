cf12ddef20d9521c68247a20763de1246ab149ce
Luc Ho <hotriluc97@gmail.com>
2023-03-11 18:52:33 +0100

    scroll rabbit

--- src/components/home/HomeExperience.jsx
+++ src/components/home/HomeExperience.jsx
@@ -6,8 +6,12 @@ import {
   MeshTransmissionMaterial,
   TransformControls,
   useMask,
+  useScroll,
 } from "@react-three/drei";
+import { useFrame } from "@react-three/fiber";
 import { useControls } from "leva";
+import { useRef } from "react";
+import { damp } from "three/src/math/MathUtils";
 
 import { RoundedPlaneShape } from "../../helpers/RoundPlaneShape";
 import Background from "../Background";
@@ -16,7 +20,45 @@ import { Bunny } from "../models/Bunny";
 const planeShape = new RoundedPlaneShape();
 
 const HomeExperience = () => {
-  const stencil = useMask(1);
+  const sphereRef = useRef();
+  const sphereRef2 = useRef();
+
+  const { scroll } = useScroll();
+
+  useFrame((state, delta) => {
+    sphereRef.current.rotation.y = damp(
+      sphereRef.current.rotation.y,
+      0 + scroll.current * 10,
+      3,
+      delta
+    );
+    sphereRef2.current.rotation.y = damp(
+      sphereRef2.current.rotation.y,
+      0 + scroll.current * 13,
+      3,
+      delta
+    );
+
+    sphereRef.current.position.y = damp(
+      sphereRef.current.position.y,
+      0 + scroll.current * 2,
+      1.5,
+      delta
+    );
+
+    sphereRef2.current.position.y = damp(
+      sphereRef2.current.position.y,
+      -2 - scroll.current * 21,
+      1.5,
+      delta
+    );
+
+    sphereRef2.current.scale.x =
+      sphereRef2.current.scale.z =
+      sphereRef2.current.scale.y =
+        damp(sphereRef2.current.scale.x, 1 + scroll.current * 0.85, 1.2, delta);
+  });
+
   const config = useControls({
     meshPhysicalMaterial: false,
     transmissionSampler: false,
@@ -43,11 +85,30 @@ const HomeExperience = () => {
       <Background />
 
       <Float>
-        <mesh position={[0, 0, 2]}>
+        <mesh
+          ref={sphereRef}
+          position={[0, 5, 2]}
+          rotation={[0, Math.PI * 4, 0]}
+        >
           <sphereGeometry />
           <MeshTransmissionMaterial {...config} />
           <Center>
             <Bunny scale={0.5} />
+          </Center>
+        </mesh>
+      </Float>
+
+      <Float rotationIntensity={0.3} floatIntensity={5}>
+        <mesh
+          ref={sphereRef2}
+          position={[2, -4, 1]}
+          rotation={[0, -Math.PI * 2, 0]}
+        >
+          <sphereGeometry args={[0.6, 8, 16]} />
+          <MeshTransmissionMaterial {...config} />
+
+          <Center>
+            <Bunny scale={0.25} />
           </Center>
         </mesh>
       </Float>
